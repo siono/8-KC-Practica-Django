0aad9f486deebb934766de3a3f2106ee75719a25
José Carlos <jc.esteban.gomez@gmail.com>
2018-01-02 14:00:35 +0100

    Creamos los modelos de Blog, category y post y generamos la migraciones

--- src/blogs/models.py
+++ src/blogs/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self): # este método tiene 0 parametros
+        """
+        Devuelve la representación de un objeto como un string
+
+        """
+        return self.name
+
+class Blog(models.Model):
+
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+
+
+    def __str__(self): # este método tiene 0 parametros
+        """
+        Devuelve la representación de un objeto como un string
+
+        """
+        return self.name
+
+class Post(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE) #on_delete=models.CASCADE = Si se borra el usuario se borra todas las post que haya realizado.
+
+    title = models.CharField(max_length=150)
+    summary = models.CharField(max_length=150)
+    body = models.TextField()
+
+    multimedia = models.URLField(blank=True, null=True)
+
+    publication_date = models.DateTimeField()
+
+    category = models.ManyToManyField(Category)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    def __str__(self):  # este método tiene 0 parametros
+
+        return self.title
