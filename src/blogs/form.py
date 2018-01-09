c4105dc881ae786053f4ae88f91fd742a1bef13f
José Carlos <jc.esteban.gomez@gmail.com>
2018-01-09 11:25:19 +0100

    Añadido estilos al formulario de creación de post

--- src/blogs/form.py
+++ src/blogs/form.py
@@ -1,10 +1,18 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, forms
+from django.forms.widgets import Input
 
 from blogs.models import Post
 
 
 class PostForm(ModelForm):
+
     class Meta:
             model = Post
             fields = '__all__'
             exclude = ["user","blog"] #exclude el campo user y blog en el formulario
+
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
