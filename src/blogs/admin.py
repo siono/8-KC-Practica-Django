1f8f6a3f38bc94140c63b0e73cce5a90b89f729b
José Carlos <jc.esteban.gomez@gmail.com>
2018-01-13 13:01:32 +0100

    Cambios en el administrador de Django para gestionar mejor los modelos

--- src/blogs/admin.py
+++ src/blogs/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 
 from blogs.models import Category, Blog, Post
 
-admin.site.register(Category)
-admin.site.register(Blog)
-admin.site.register(Post)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_select_related = ('user',)
+    list_display = ('name', 'user')
+    search_fields = ('name', 'user__first_name', 'user__last_name', 'user__email', 'user__username')
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    fields = ('title', 'multimedia', 'summary', 'body', ('publication_date', 'blog'), 'categories')
+    filter_horizontal = ('categories',)
+    date_hierarchy = 'publication_date'
+    search_fields = ('title', 'summary', 'body', 'blog__name')
+    list_select_related = ('blog',)
+    list_filter = ('categories', 'blog')
+    list_display = ('title', 'blog', 'publication_date')
+    save_as = True
